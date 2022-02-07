97013f373bce636cfdbf83da1edc54b35041ab46
kandelin16 <78173482+kandelin16@users.noreply.github.com>
2022-02-06 20:02:15 -0700

    worked on react frontend

---
+++ frontend/src/components/login.js
@@ -0,0 +1,13 @@
+import react from "react";
+import {Switch, Route, Link} from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function Login() {
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default Login;
